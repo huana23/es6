eac9050c0418e2d3172c51264224140958c20403
An <anaquacu@gmail.com>
2022-04-08 00:09:10 +0700

    note

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-document.querySelector('#addItem').onclick = function(){
-    if(document.querySelector('#newTask').value.length == 0){
+document.querySelector('#addItem').onclick = function () {
+    if (document.querySelector('#newTask').value.length == 0) {
         alert("Cần điền việc cần làm ...")
     }
 
-    else{
+    else {
         document.querySelector('#todo').innerHTML += `
             <li class="task">
                 <span id="taskname">
                     ${document.querySelector('#newTask').value}
                 </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-
-                <button class="check">
-                    <i class="fa fa-check"></i>
-                </button>
+                
+                    <button class="delete">
+                        <i class="far fa-trash-alt"></i>
+                    </button>
+                
+                
             </li>
         `;
 
         var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
                 this.parentNode.remove();
             }
         }
